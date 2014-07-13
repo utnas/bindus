d6736a6b0dfd97dea9ccba478c5550fb11d0403d
Dooley <nsewolo@gmail.com>
2014-07-13 12:26:26 -0400

    sign up and sign in done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,17 +27,29 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+=begin
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: 'smtp.gmail.com',
     port: 587,
     domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
+    authentication: 'plain',
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
   }
+=end
+
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :user_name            => 'nsewolo@gmail.com',
+      :password             => 'Mfv2tlm2',
+      :authentication       => 'login',
+      :enable_starttls_auto => true
+  }
+
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
