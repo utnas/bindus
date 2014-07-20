e4dede228a87d86ce5f4785b97f237bff804c0b4
Dooley <nsewolo@gmail.com>
2014-07-20 01:16:07 -0400

    place holder added

--- app/assets/javascripts/alert_message.js
+++ app/assets/javascripts/alert_message.js
@@ -1,1 +1,12 @@
+(function(){
+    'use strict';
 
+    $(document).ready(function () {
+        window.setTimeout(function () {
+            $('.alert').fadeTo(500, 0).slideUp(500, function () {
+                $(this).remove();
+            });
+        }, 500);
+    });
+
+})();
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,12 +3,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require alert_message
+//= require_self
 //= require_tree .
-
-$(document).ready(function () {
-    window.setTimeout(function () {
-        $('.alert').fadeTo(500, 0).slideUp(500, function () {
-            $(this).remove();
-        });
-    }, 500);
-});
