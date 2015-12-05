1003590eeb519e729667f1b1280dc7b4bb401a2c
Dooley <nsewolo@gmail.com>
2015-12-05 01:00:06 -0500

    refactoring

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,9 +1,11 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
-        user.confirm!
-      end
+    secrets = Rails.application.secrets
+
+    user = User.find_or_create_by!(email: secrets.admin_email) do |user|
+      user.password = secrets.admin_password
+      user.password_confirmation = secrets.admin_password
+      user.confirm!
+    end
   end
 end
